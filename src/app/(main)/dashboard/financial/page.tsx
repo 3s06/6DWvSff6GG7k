1690eeeb783049bcb3fd729a634705f949060ab4
3s06 <carneirolucassouza@gmail.com>
2025-05-15 19:18:05 -0300

    create: products page

--- src/app/(main)/dashboard/financial/page.tsx
+++ src/app/(main)/dashboard/financial/page.tsx
@@ -3,10 +3,61 @@
   *
 */
 
+import { NavbarDashboard } from "@/components/NavbarDashboardUsuário";
+import { HandCoinsIcon } from "@/components/ui/hand-coins";
+
 export default function PáginaDoFinanceiro() {
     return (
-        <div>
-
+        <div className="bg-white dark:bg-neutral-950 flex flex-col w-full">
+          <NavbarDashboard />
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 items-center p-2 sm:p-6">
+          <div className="bg-white dark:bg-neutral-950 group hover:bg-emerald-50 rounded-2xl sm:rounded-3xl w-[390px] p-3 sm:px-4 sm:py-4 flex items-start justify-start border border-emerald-200 dark:border-white/30 hover:border-emerald-300 dark:hover:bg-white/5">
+              <div className="bg-emerald-100 border text-emerald-500 border-emerald-400 dark:bg-transparent dark:text-white dark:border-white/50 w-[60px] h-[60px] sm:w-[55px] sm:h-[55px] rounded-xl sm:rounded-2xl flex items-center justify-center mr-3 sm:mr-4">
+                <HandCoinsIcon size={25} />
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex items-start justify-between w-full">
+                  <span className="text-black dark:text-white tracking-tight text-md font-semibold">
+                    Saldo Disponível
+                  </span>
+                </div>
+                <h1 className="text-2xl text-black tracking-tight font-bold dark:textwhite">R$ 0,00</h1>
+              </div>
+          </div>
+          <div className="bg-white dark:bg-neutral-950 group hover:bg-emerald-50 rounded-2xl sm:rounded-3xl w-[390px] p-3 sm:px-4 sm:py-4 flex items-start justify-start border border-emerald-200 dark:border-white/30 hover:border-emerald-300 dark:hover:bg-white/5">
+              <div className="bg-emerald-100 border text-emerald-500 border-emerald-400 dark:bg-transparent dark:text-white dark:border-white/50 w-[60px] h-[60px] sm:w-[55px] sm:h-[55px] rounded-xl sm:rounded-2xl flex items-center justify-center mr-3 sm:mr-4">
+                <HandCoinsIcon size={25} />
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex items-start justify-between w-full">
+                  <span className="text-black dark:text-white tracking-tight text-md font-semibold">
+                    Saldo a liberar 
+                  </span>
+                </div>
+                <h1 className="text-2xl text-black tracking-tight font-bold dark:textwhite">R$ 0,00</h1>
+              </div>
+          </div>
+          <div className="bg-white dark:bg-neutral-950 group hover:bg-emerald-50 rounded-2xl sm:rounded-3xl w-[390px] p-3 sm:px-4 sm:py-4 flex items-start justify-between border border-emerald-200 dark:border-white/30 hover:border-emerald-300 dark:hover:bg-white/5">
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex items-start justify-between w-full mb-1">
+                  <span className="text-black dark:text-white tracking-tight text-md font-semibold">
+                    Efetuar Saque
+                  </span>
+                </div>
+                <p className="text-sm text-gray-500 tracking-tight font-medium dark:text-neutral-500">O dinheiro cairá na sua conta em até 1 dia útil</p>
+              </div>
+              <div className="bg-emerald-100 border text-emerald-500 border-emerald-400 dark:bg-transparent dark:text-white dark:border-white/50 w-[60px] h-[60px] sm:w-[55px] sm:h-[55px] rounded-xl sm:rounded-2xl flex items-center justify-center ">
+                <HandCoinsIcon size={25} />
+              </div>
+          </div>
+          </div>
+          <div className="flex p-2 sm:p-6">
+            <div className="bg-neutral-200 w-full border-b border-neutral-400 items-center justify-center rounded-b-none rounded-xl p-2 sm:p-3 py-4 grid grid-cols-3">
+                <span className="text-neutral-600 font-medium tracking-tight text-base">Data</span>
+                <span className="text-neutral-600 font-medium tracking-tight text-base">Valor líquido</span>
+                <span className="text-neutral-600 font-medium tracking-tight text-base">Status</span> 
+            </div>
+          </div>
         </div>
     )
 }
--- src/app/(main)/dashboard/reports/page.tsx
+++ src/app/(main)/dashboard/reports/page.tsx
@@ -5,26 +5,46 @@
 
 import Link from "next/link";
 import { HandCoinsIcon } from "@/components/ui/hand-coins";
+import { MdArrowForward  } from "react-icons/md";
+import { NavbarDashboard } from "@/components/NavbarDashboardUsuário";
+import { BoxesIcon } from "@/components/ui/boxes";
+import { ArchiveIcon } from "@/components/ui/archive";
+import { CartIcon } from "@/components/ui/cart";
+import { BlocksIcon } from "@/components/ui/blocks";
+import { WorkflowIcon } from "@/components/ui/workflow";
+
+const MapaDeRelatorios = [
+  { name: 'Vendas Realizadas', icon: HandCoinsIcon, description: ' Lista de todas as vendas concluídas.', url: '/reports/sales'},
+  { name: 'Vendas Abandonas', icon: ArchiveIcon, description: ' Analise as taxas e as causas das vendas não finalizadas.', url: '/reports/abandoned-sales'},
+  { name: 'Receita por Produto/Serviço', icon: BoxesIcon, description: ' Analise quanto cada item gerou de receita em um período.', url: '/reports/recipe-product'},
+  { name: 'Produtos Mais Vendidos', icon: CartIcon, description: 'Lista os produtos mais vendidos no período.', url: '/reports/vendas-abandonas'},
+  { name: 'Estornos e Reembolsos', icon: BlocksIcon, description: ' Lista de vendas que foram reembolsadas ou canceladas', url: '/reports/vendas-abandonas'},
+  { name: 'Conversão de Pagamentos', icon: WorkflowIcon, description: 'Mostra quantas pessoas: Iniciaram o checkout e outros', url: '/reports/vendas-abandonas'},
+]
 
 export default function PáginaDeRelatórios() {
   return (
-    <div className="bg-white dark:bg-neutral-950 flex flex-col w-full ">
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-4 items-center">
-        <div className="bg-white hover:bg-emerald-50 rounded-2xl px-3 py-2 flex items-start justify-start border border-emerald-100 hover:border-emerald-300">
-          <div className="bg-emerald-100 border text-emerald-500 border-emerald-400 size-10 rounded-xl flex items-center justify-center mr-2.5">
-            <HandCoinsIcon size={22} />
-          </div>
-          <div className="flex flex-col items-start justify-start">
-            <div className="flex items-start justify-start w-full">
-              <span className="text-black tracking-tight font-semibold">
-                Vendas Abandondas
-              </span>
-            </div>
-            <p className="text-xs text-neutral-500 hover:text-emerald-300">
-              Analise as taxas e as causas das vendas não finalizadas.
-            </p>
-          </div>
-        </div>
+    <div className="bg-white dark:bg-neutral-950 flex flex-col w-full">
+      <NavbarDashboard />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 items-center p-2 sm:p-6">
+        {MapaDeRelatorios.map((link) => (
+          <Link href={link.url} className="bg-white dark:bg-neutral-950 group hover:bg-emerald-50 rounded-2xl sm:rounded-3xl w-[390px] p-3 sm:px-4 sm:py-4 flex items-start justify-start border border-emerald-200 dark:border-white/30 hover:border-emerald-300 dark:hover:bg-white/5">
+              <div className="bg-emerald-100 border text-emerald-500 border-emerald-400 dark:bg-transparent dark:text-white dark:border-white/50 w-[60px] h-[60px] sm:w-[55px] sm:h-[55px] rounded-xl sm:rounded-2xl flex items-center justify-center mr-3 sm:mr-2.5">
+                <link.icon size={25} />
+              </div>
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex items-start justify-between w-full">
+                  <span className="text-black dark:text-white tracking-tight text-md font-semibold">
+                    {link.name}
+                  </span>
+                  <MdArrowForward size={22} className="text-black dark:text-white " />
+                </div>
+                <p className="text-sm sm:text-xs text-neutral-500 dark:text-neutral-400 group-hover:text-emerald-500 dark:group-hover:text-white">
+                  {link.description}
+                </p>
+              </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
